a3cb86772accdd5fa6bd45ca977e766237a18bf6
Ali Imam <visual.imam@gmail.com>
2024-06-08 12:19:35 +0530

    apl

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['ai-eslints/index.js'],
+  extends: ['ai-eslints/library.js'],
   parserOptions: {
     project: './tsconfig.json',
     tsconfigRootDir: __dirname
